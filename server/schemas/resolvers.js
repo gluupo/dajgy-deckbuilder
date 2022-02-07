a527709b5dc0d1247506ec4897be4400ff138313
Yonas Woldemichael <ybyonas@gmail.com>
2022-02-07 10:23:24 -0500

    fixed login func.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { card } = require('mtgsdk')
+const { signToken } = require('../utils/auth')
 
 const resolvers = {
   Query: {
