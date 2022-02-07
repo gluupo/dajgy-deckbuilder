f525b1143c91a500a40f6c1f8189c5a14cb886c0
The Beefy Mang <jesus.rosa75@yahoo.com>
2022-02-07 11:03:12 -0500

    working on seed.js

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,11 +1,15 @@
 const db = require('../config/connection');
 const { User } = require('../models');
+const { Deck } = require('../models');
 const userSeeds = require('./userSeeds.json');
+const deckSeeds = require('./deckSeeds.json');
 
 db.once('open', async () => {
   try {
     await User.deleteMany({});
+    await Deck.deleteMany({});
     await User.create(userSeeds);
+    await Deck.create(deckSeeds);
   } catch (err) {
     console.error(err);
     process.exit(1);
