b14ac53afcff3dcf4ef381dbf847af4274012b76
The Beefy Mang <jesus.rosa75@yahoo.com>
2022-02-08 13:49:37 -0500

    changes to edit deck

--- client/src/pages/Profile/DeckList.js
+++ client/src/pages/Profile/DeckList.js
@@ -1,17 +1,28 @@
 // Node Modules
 import React from 'react';
-import { ListGroup } from 'react-bootstrap'
+import { ListGroup, Button } from 'react-bootstrap'
+import { EDIT_DECK } from '../../utils/mutations';
+import { useMutation } from '@apollo/client';
 
 const DeckList = ({ deck }) => {
+    const [editDeck, { error, data }] = useMutation(EDIT_DECK);
     console.log("DECK: ", deck)
 
     const viewDeckDetails = (deck) => {
         // TODO: Get details of selected deck
     }
 
+    const editDeckHandler = async () => {
+        const { data } = await editDeck({
+            variables: { deckId: deck._id }
+        })
+    }
+
     return (
-        <ListGroup.Item action onClick={viewDeckDetails}>
+        <ListGroup.Item action >
             {deck.name}
+            <Button onClick={viewDeckDetails}>view</Button>
+            <Button onClick={editDeckHandler}>edit</Button>
         </ListGroup.Item>
     );
 };
