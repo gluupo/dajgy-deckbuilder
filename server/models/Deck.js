7ac2484c0b237226c0014ab07bcee6a056c02e28
The Beefy Mang <jesus.rosa75@yahoo.com>
2022-02-08 10:24:51 -0500

    cardCount virtual created

--- server/models/Deck.js
+++ server/models/Deck.js
@@ -1,48 +1,57 @@
 const { Schema, model } = require('mongoose');
 
-const deckSchema = new Schema({
-  name: {
-    type: String,
-  },
-  cards: [
-    {
-      name: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      types: [
-        {
+const deckSchema = new Schema(
+  {
+    name: {
+      type: String,
+    },
+    cards: [
+      {
+        name: {
           type: String,
           required: true,
-        }
-      ],
-      manaCost: {
-        type: String,
-      },
-      superTypes: [
-        {
+          unique: true,
+        },
+        types: [
+          {
+            type: String,
+            required: true,
+          }
+        ],
+        manaCost: {
+          type: String,
+        },
+        superTypes: [
+          {
+            type: String,
+          }
+        ],
+        rarity: {
+          type: String,
+        },
+        imageUrl: {
+          type: String,
+          required: true,
+        },
+        text: {
+          type: String,
+        },
+        multiverseid: {
           type: String,
         }
-      ],
-      rarity: {
-        type: String,
       },
-      imageUrl: {
-        type: String,
-        required: true,
-      },
-      text: {
-        type: String,
-      },
-      multiverseid: {
-        type: String,
-      },
-      cardCount: {
-        type: Number
-      }
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
     },
-  ],
+    id: false
+  }
+);
+
+deckSchema.virtual('cardCount').get(function () {
+  return this.cards.length;
 });
 
 const Deck = model('Deck', deckSchema);
