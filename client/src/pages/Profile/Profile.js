9668e4fff23fbe2b94516c8a6d4d3a08dfdc4831
89209062+dcrlsn@users.noreply.github.com <c4r150n@gmail.com>
2022-02-10 11:05:29 -0500

    readded create button

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -90,55 +90,56 @@ const Profile = () => {
   }
 
   const renderCreateButton = () => {
-    if (Auth.loggedIn() && Auth.getProfile().data._id === id) {
-      return (
-        <Button variant="outline-light" onClick={createHandler}>Create Deck</Button>
-      )
-    }
+    if (id) return null
+
+    return (
+      <Button variant="outline-light" onClick={createHandler}>Create Deck</Button>
+    )
   }
+}
 
 
-  const renderDeckList = () => {
-    if (!user.decks.length) {
-      return (
-        <h4>No Decks Created</h4>
-      )
-    } else {
-      console.log(user.decks.length)
-      return (
-        <ListGroup defaultActiveKey="key">
-          {
-            user.decks.map(deck => (
-              <DeckList key={deck._id} {...deck} workingDeck={user.workingDeck} />
-            ))
-          }
-        </ListGroup>
-      )
-    }
-  };
+const renderDeckList = () => {
+  if (!user.decks.length) {
+    return (
+      <h4>No Decks Created</h4>
+    )
+  } else {
+    console.log(user.decks.length)
+    return (
+      <ListGroup defaultActiveKey="key">
+        {
+          user.decks.map(deck => (
+            <DeckList key={deck._id} {...deck} workingDeck={user.workingDeck} />
+          ))
+        }
+      </ListGroup>
+    )
+  }
+};
 
-  return (
-    <>
-      <Container className='mt-5'>
-        <Row>
+return (
+  <>
+    <Container className='mt-5'>
+      <Row>
+        <div>
           <div>
-            <div>
-              <h2>
-                Viewing {id ? `${user.username}'s` : 'your'} profile.
-              </h2>
-              {renderCurrentUserInfo()}
-              {renderCreateButton()}
-            </div>
+            <h2>
+              Viewing {id ? `${user.username}'s` : 'your'} profile.
+            </h2>
+            {renderCurrentUserInfo()}
+            {renderCreateButton()}
           </div>
-          <Col xs={12}>
-            {renderDeckList()}
-          </Col>
-          <Col xs lg="2">
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
+        </div>
+        <Col xs={12}>
+          {renderDeckList()}
+        </Col>
+        <Col xs lg="2">
+        </Col>
+      </Row>
+    </Container>
+  </>
+);
 };
 
 export default Profile;
