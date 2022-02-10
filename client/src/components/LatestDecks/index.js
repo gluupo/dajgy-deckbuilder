1ddf1133fe318e11f00a92a56f9fb6e575926713
89209062+dcrlsn@users.noreply.github.com <c4r150n@gmail.com>
2022-02-10 10:35:19 -0500

    fixed latest deck rendering

--- client/src/components/LatestDecks/index.js
+++ client/src/components/LatestDecks/index.js
@@ -4,15 +4,15 @@ import { useQuery } from '@apollo/client';
 import { GET_ALL_DECKS } from '../../utils/queries';
 
 const LatestDeck = () => {
-  const { loading, data } = useQuery(GET_ALL_DECKS);
-  const decks = data?.decks || [];
-
+  const { loading, data } = useQuery(GET_ALL_DECKS, { variables: { order: -1, limit: 5 } });
+  const decks = data?.getAllDecks || [];
 
 
   const renderDecks = () => {
     if (loading) {
       return <h2>Loading ...</h2>
     } else if (!decks.length) return null;
+    console.log(decks);
     return decks.map(deck => <div>{deck.name}</div>)
   }
 
