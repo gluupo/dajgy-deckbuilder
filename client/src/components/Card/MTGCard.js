6f2c2d0851e3bcfdc9369e4b7adc2e5442b490d8
89209062+dcrlsn@users.noreply.github.com <c4r150n@gmail.com>
2022-02-11 00:05:49 -0500

    pushing changes to heroku

--- client/src/components/Card/MTGCard.js
+++ client/src/components/Card/MTGCard.js
@@ -23,8 +23,8 @@ const MTGCard = (item) => {
     cardCount,
     deckId
   } = item;
-  const { loading, data, error: deckError } = useQuery(GET_DECK, { variables: { _id: id || '6205cea3a8acc60016fefc87' } });
-  const deck = data?.getDeck.cards || {}
+  const { loading, data, error: deckError } = useQuery(GET_DECK, { variables: { _id: '6205cea3a8acc60016fefc87' } });
+  const deck = data?.getDeck || {}
 
   const [addToDeck, { error }] = useMutation(ADD_TO_DECK, {
     refetchQueries: [
