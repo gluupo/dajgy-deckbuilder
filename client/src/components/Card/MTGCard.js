e6cd75d65847027e889f02ac558513970e3405a1
89209062+dcrlsn@users.noreply.github.com <c4r150n@gmail.com>
2022-02-10 21:16:57 -0500

    fixed resolver

--- client/src/components/Card/MTGCard.js
+++ client/src/components/Card/MTGCard.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Col, Button } from "react-bootstrap"
-import { useMutation } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import Auth from "../../utils/auth";
+import { GET_DECK } from "../../utils/queries";
 import { ADD_TO_DECK, REMOVE_FROM_DECK } from "../../utils/mutations";
 import './assets/styles.css';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
@@ -9,7 +10,7 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 
 const MTGCard = (item) => {
-  const {
+  let {
     multiverseid,
     text,
     manaCost,
@@ -19,15 +20,30 @@ const MTGCard = (item) => {
     imageUrl,
     types,
     editable,
-    cardCount
+    cardCount,
+    deckId
   } = item;
+  const { loading, data, error: deckError } = useQuery(GET_DECK, { variables: { _id: deckId } });
+  const deck = data?.getDeck.cards || {}
 
-  const [addToDeck, { error }] = useMutation(ADD_TO_DECK);
-  const [removeCard, { removeError }] = useMutation(REMOVE_FROM_DECK);
+  const [addToDeck, { error }] = useMutation(ADD_TO_DECK, {
+    refetchQueries: [
+      GET_DECK, // DocumentNode object parsed with gql
+      'getDeck' // Query name
+    ],
+  });
+
+  const [removeCard, { removeError }] = useMutation(REMOVE_FROM_DECK, {
+    refetchQueries: [
+      GET_DECK, // DocumentNode object parsed with gql
+      'getDeck' // Query name
+    ],
+  });
 
   const clickHandler = async (e) => {
     try {
       // Execute mutation and pass in defined parameter data as variables
+
       const copy = {
         multiverseid,
         text,
@@ -38,7 +54,8 @@ const MTGCard = (item) => {
         imageUrl,
         types,
       }
-      // deletes key from object
+      console.log(copy)
+
       if (e.target.dataset.type === '+') {
         const { data } = await addToDeck({
           variables: { input: copy }
@@ -49,7 +66,6 @@ const MTGCard = (item) => {
         })
       }
     } catch (err) {
-      console.error(err);
     }
   }
 
@@ -90,7 +106,7 @@ const MTGCard = (item) => {
 
   return (
     <Col xs={12} sm={6} md={3} key={name} className='mb-3 m-1'>
-      <h4 className="text-white">{cardCount} in Deck</h4>
+      {cardCount ? <h4 className="text-white">{cardCount} in Deck</h4> : null}
       <img src={imageUrl} className="mtg-card" alt={`${name} ${manaCost} \n${text}`} title={`${name} ${manaCost} \n${text}`} />
       {renderButtons()}
     </Col >
